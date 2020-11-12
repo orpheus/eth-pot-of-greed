9069bf211aed2dd6489770421672d8bbb1754698
ohryan <ryanachacon@gmail.com>
2020-11-12 15:54:37 -0700

    init commit

--- contracts/PotOfGreed.sol
+++ contracts/PotOfGreed.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 contract Utility {
-  address owner;
+  address payable owner;
 
   event errorEvent(string err);
 
@@ -20,9 +20,35 @@ contract PotOfGreed is Utility {
     owner = msg.sender;
   }
 
-  uint public pot = 0;
+  uint256 public pot = 0;
+  uint public constant minimumInput = 1;
+  uint public lastGreed;
 
-  function updatePot(uint value) public {
+  
+  function greed() public payable {
+      require(msg.value > minimumInput, "Not enough money sent. Need > 1 wei");
+      lastGreed = msg.value;
+      updatePot(msg.value);
+      roll();
+  }
+  
+  function updatePot(uint value) internal {
     pot = pot + value;
+  }
+  
+  function roll () internal {
+      if (msg.sender == owner) {
+          payout();
+      }
+  }
+  
+  function payout() internal {
+      uint256 payoutAmt = (pot * 9 / 10);
+      pot = pot - payoutAmt;
+      msg.sender.transfer(payoutAmt);
+  }
+  
+  function destroy () onlyOwner public {
+      selfdestruct(owner);
   }
 }
--- test/PotOfGreed.test.js
+++ test/PotOfGreed.test.js
@@ -4,10 +4,10 @@ contract("PotOfGreed", accounts => {
   it("should update the pot", async () => {
     const pog = await PotOfGreed.deployed();
 
-    await pog.updatePot(3, { from: accounts[0] });
+    // await pog.greed({ from: accounts[0] });
 
-    const pot = await pog.pot.call();
+    // const pot = await pog.pot.call();
 
-    assert.equal(pot, 3, "The pot was updated");
+    // assert.equal(pot, 3, "The pot was updated");
   });
 });
